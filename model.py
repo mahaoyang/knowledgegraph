61208c763fddf6421f2b7d28f58d374538dbf8b6
99263 <992635910@qq.com>
2018-11-01 23:54:26 +0800

    init

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import pickle
 
 import numpy as np
 
+from keras import losses
 from keras import utils
 from keras import layers
 from keras import models
@@ -16,18 +17,27 @@ MAX_WORD_INDEX = 3244
 
 
 def model():
-    passage_input = layers.Input(shape=(MAX_PASSAGE_LENGTH,), dtype='int32')
+    passage_input = layers.Input(shape=(MAX_PASSAGE_LENGTH,), dtype='int16')
     passage = layers.Embedding(MAX_WORD_INDEX + 1,
                                300,
                                # weights=[embedding_matrix],
                                input_length=MAX_PASSAGE_LENGTH,
                                trainable=False)(passage_input)
     passage = Position_Embedding()(passage)
-    p_encoder = layers.Bidirectional(layers.LSTM(2, return_sequences=True))(passage)
-    p_encoder = layers.Bidirectional(layers.LSTM(2, return_sequences=True))(p_encoder)
-
+    # p_encoder = layers.Conv1D(32, 7, activation='relu', padding='same')(passage)
+    # p_encoder = layers.MaxPooling1D()(p_encoder)
+    # p_encoder = layers.Conv1D(64, 7, activation='relu', padding='same')(p_encoder)
+    # p_encoder = layers.MaxPooling1D()(p_encoder)
+    # p_encoder = layers.Conv1D(128, 7, activation='relu', padding='same')(p_encoder)
+    # p_encoder = layers.MaxPooling1D()(p_encoder)
+    # p_encoder = layers.Conv1D(256, 7, activation='relu', padding='same')(p_encoder)
+    # p_encoder = layers.MaxPooling1D()(p_encoder)
+    # p_encoder = layers.Conv1D(512, 7, activation='relu', padding='same')(p_encoder)
+    # p_encoder = layers.MaxPooling1D()(p_encoder)
+    p_encoder = layers.Bidirectional(layers.GRU(32, return_sequences=True))(passage)
     p_encoder = layers.MaxPooling1D()(p_encoder)
-    p_encoder = layers.Flatten()(p_encoder)
+
+    p_encoder = layers.GlobalMaxPooling1D()(p_encoder)
 
     # a_decoder = Attention(1, 4)([p_encoder, q_encoder, alt_encoder])
     # a_decoder = layers.Flatten()(a_decoder)
@@ -39,7 +49,7 @@ def model():
 
     rc_model = models.Model(inputs=passage_input, outputs=output)
     opti = optimizers.Adam(lr=1e-1, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-    rc_model.compile(optimizer=opti, loss="categorical_crossentropy", metrics=["accuracy"])
+    rc_model.compile(optimizer=opti, loss=losses.msle, metrics=["accuracy"])
 
     rc_model.summary()
     return rc_model
