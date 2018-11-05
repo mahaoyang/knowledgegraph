63acace9fa4d6d520143caddc0968f0373e05da6
99263 <992635910@qq.com>
2018-11-05 12:51:07 +0800

    init

--- run.py
+++ run.py
@@ -46,10 +46,10 @@ def dgen_v(batch_size=32):
 
 
 def train():
-    batch_size = 16
+    batch_size = 2
     m = model()
     # m.load_weights('1.h5')
-    m.fit_generator(dgen(batch_size), steps_per_epoch=300, epochs=60, validation_data=dgen_v(batch_size),
+    m.fit_generator(dgen(batch_size), steps_per_epoch=20, epochs=50, validation_data=dgen_v(batch_size),
                     validation_steps=20)
     m.save_weights('1.h5')
 
